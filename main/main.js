d4f70600485f96d2f94e3e37300668b6756d8ac0
yaozhihan1994 <yaozhihan1994@163.com>
2019-05-23 22:59:27 +0800

    first commit, finshed

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+	var d = inputs.distance; 
+	var p = inputs.parkTime;
+	var m = 0;
+	if(d <= 2) m = 6 + p*0.25;
+	else if(d > 2 && d <= 8) m = 6 + p*0.25 + (d-2)*0.8;
+	else if(d > 8) m = 10.8 + p*0.25 + (d-8)*1.2;
+    return Math.round(m);
 };
